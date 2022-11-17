be287324daa6888021535b5f2c0ab1649e82f516
Pravin <prayas2624.com>
2022-11-17 18:55:12 +0530

    my second command

--- Demo2/src/com/velocity/Demo_Example.java
+++ Demo2/src/com/velocity/Demo_Example.java
@@ -4,6 +4,7 @@ public class Demo_Example {
 
 	public static void main(String[] args) {
 		System.out.println("pravin ankur");
+		System.out.println("program uploaded successfully on git hub");
 
 	}
 
